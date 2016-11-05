0e4e069f0d81aca8925bfd37720a9a53aeda738b
Tucker <tucker.r.chapman@gmail.com>
2016-11-05 17:23:10 -0600

    Change summary length and add subtitle

--- models.py
+++ models.py
@@ -9,9 +9,10 @@ from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=300, default="Setting People Free")
     pub_date = models.DateTimeField('date published')
     body = models.TextField()
-    summary = models.CharField(max_length=200)
+    summary = models.CharField(max_length=250, default="This will be replaced by the first 250 characters of the body text")
     def __str__(self):
         return self.title
     def was_published_recently(self):
@@ -19,7 +20,7 @@ class Post(models.Model):
         return now - datetime.timedelta(days=7) <= self.pub_date <= now
     def save(self, *args, **kawgs):
         if self.body:
-            self.summary = self.body[:500] + '...'
+            self.summary = self.body[:245] + '...'
         super(Post, self).save(*args, **kawgs)
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
